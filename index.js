b4d40bfd605c73c031eac9d7a52f18925b1fa488
anjaedman <113342171+anjaedman@users.noreply.github.com>
2023-01-16 15:02:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
-
-let totalEl = document.getElementById("total-el")
+let textTotal = document.getElementById("total")
+// let totalEl = document.getElementById("total-el")
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el");
 // grab the count-el element, store it in a countEl variable
 let count = 0
+let countTotal = []
+let showTotal;
 
 function increment() {
     count += 1
@@ -16,8 +18,12 @@ function save() {
     let countString = count + " - "
     saveEl.textContent += countString
     countEl.textContent = 0
+    countTotal.push(count)
+    showTotal = countTotal.reduce((partialSum, a) => partialSum + a, 0)
+    textTotal.textContent = "Total: " + showTotal
     count = 0
 }
+
 
 
 
